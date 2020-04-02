2cb0691ebe78aa6e63cddc80a493e3a6d6b3fe49
umarabdulsattar <umarabdulsattar3275@gmail.com>
2020-04-02 20:13:45 +0200

    Now i am woking with postgreSQL database and creat database which name is 'travller' and using psycopg2 adopter i connect my django app  with travller databse and  with migration commend icreate table in database

--- travller/travller/settings.py
+++ travller/travller/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'HomePage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'travller.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'travller',
+        'USER':'postgres',
+        'PASSWORD':'3275',
+        'HOST':'localhost',
+        'PORT': '3275',
     }
 }
 
